2e8722e4f050bdbef2d299aabdfb9c4a56b2abd8
Tony <waiting33118@gmail.com>
2020-04-28 20:16:29 +0800

    initial project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const hostname = `127.0.0.1`
+const port = 3000
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.render('home', { restaurant: restaurantList.results })
+})
+
+app.get('/show', (req, res) => {
+	res.render('show')
+})
+
+app.listen(port, hostname, () => {
+	console.log(`The Server is running on http://${hostname}:${port}`)
+})
